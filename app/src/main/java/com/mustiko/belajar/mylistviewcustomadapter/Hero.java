9268ee8faab415d37ae43743f73afd66067099e6
Kunto Mustiko <kuntomustiko@gmail.com>
2019-11-19 21:35:50 +0700

    Initial commit

---
+++ app/src/main/java/com/mustiko/belajar/mylistviewcustomadapter/Hero.java
@@ -0,0 +1,34 @@
+package com.mustiko.belajar.mylistviewcustomadapter;
+
+public class Hero {
+    // todo 2
+    private int photo;
+    private String name;
+    private String description;
+
+    // hasil setter and getter hasil generate
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
